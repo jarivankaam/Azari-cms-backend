a79e36d39d5e315595ae2393258aa2269c27a45d
jarivankaam <D281531@edu.curio.nl>
2024-08-09 11:53:56 +0200

    added controllers for content content-types and sites

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { SitesController } from './sites/sites.controller';
+import { ContentTypesController } from './content-types/content-types.controller';
+import { ContentController } from './content/content.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController],
+  controllers: [AppController, SitesController, ContentTypesController, ContentController],
   providers: [AppService],
 })
 export class AppModule {}
